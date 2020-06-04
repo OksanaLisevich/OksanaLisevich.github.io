36eabcf4b296ce865c62f95a57d30a7c33c193ef
Oksana <417ksenia@gmail.com>
2020-06-04 14:07:38 +0300

    Creation

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+if (isset($_POST) && ($_SERVER['HTTP_X_HTTP_METHOD'] == 'POST'))
+{
+  $data = $_POST;
+  $http_data = http_build_query(array($_POST));
+  $status = array(
+            200 => 'OK');
+  http_response_code(200);
+  $opts = array(
+    'http'=>array(
+    'method'=>"POST",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n".
+              "Content-type: application/json\r\n",
+    'content' => $http_data
+  )
+);
+
+$url = "https://www.corezoid.com/api/1/json/public/672129/aeb696d85d651ad493c48e1a5ee8beb617eb5cbf";
+
+$context = stream_context_create($opts);
+
+
+$fp = fopen($url, 'r', false, $context);
+fpassthru($fp);
+fclose($fp);
+}
+
+?>
